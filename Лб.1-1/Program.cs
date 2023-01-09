36671671e2f23d15988f1c20d8e116cd416f7808
1izgoi <danilhomcik561@gmail.com>
2023-01-09 23:39:54 +0300

    Initial commit

---
+++ Лб.1-1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("a= ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("b= ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("{0}+{1}={2}", a, b, a + b);
+        }
+    }
+}
